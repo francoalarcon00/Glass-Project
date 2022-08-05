8bade9987d927f683a45ae3bb420e086f9e73482
Franco Alarcon <franco3.fa@gmail.com>
2022-08-05 11:31:58 -0300

    Delete scripts

--- lib_model.py
+++
@@ -1,5 +0,0 @@
-# Importamos librerias necesarias para modelado
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
--- lib_vis.py
+++
@@ -1,5 +0,0 @@
-# Importamos librerias necesarias para visualización
-
-import pandas as pd
-import matplotlib.pyplot as plt  
-import seaborn as sns
